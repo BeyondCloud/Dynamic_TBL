e986e059c3d03236608331ff222f94ab58f45a2b
BeyondCloud <a1989796@yahoo.com.tw>
2016-05-22 23:16:01 +0800

    implement dynamic table

--- DynamicTBL.cpp
+++ DynamicTBL.cpp
@@ -7,20 +7,39 @@ using namespace std;
 #define CONTRACT_RATIO 0.25
 void TBL_INSERT(int *, int);
 void TBL_DELETE(int *, int);
+void TBL_EXPAND(int *);
 int *tbl = new int[5];
-int num = 0; // number of element inside tbl
+int num = 0;      // number of element inside tbl
+int tbl_size = 1;
 main(){
+    TBL_EXPAND(tbl);
+    tbl[1] = 123;
+    cout<<tbl[1];
+    /*
     TBL_INSERT(tbl,123);
     cout<<tbl[0]<<endl;
     TBL_DELETE(tbl,123);
     if(tbl[0])
     cout<<tbl[0];
+    */
 }
 void TBL_INSERT(int *tbl,int key){
-
+    if( num /(float)tbl_size > EXPAND_RATIO)
+    {
+     //   TBL_EXPAND(int tbl);
+    }
     tbl[num++] = key;
 }
-
+void TBL_EXPAND(int *tbl){
+        int *new_tbl = new int [tbl_size* 2];
+        int index=0;
+        while(index != tbl_size)
+        {
+            new_tbl[index] = tbl[index];
+            index++;
+        }
+        tbl = new_tbl;
+}
 //search inside tbl
 //if find tbl[index] = key , then swap x with last element inside
 //array , delete last element
